c532e40b4eee09990e75dd0fac7a1ddc8db53fda
Daniel Perez <workdevelopment001@gmail.com>
2024-11-23 14:22:08 -0600

    Update api

---
+++ api/index.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example Title</title>
+    </head>
+    <body>
+        <p>
+            Hello. Today is <?= date('l \t\h\e jS') ?>.
+        </p>
+    </body>
+</html>
