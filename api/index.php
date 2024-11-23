7a5cbb4f50ecfbd12236e17634e2b856143c3ee3
Daniel Perez <workdevelopment001@gmail.com>
2024-11-23 16:10:39 -0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -9,5 +9,6 @@
             Hello. Today is <?= date('l \t\h\e jS') ?>.
         </p>
         <iframe src="localhost/cws" width="600px" height="500px"></iframe>
+        <iframe src="localhost/cws" width="600px" height="500px"></iframe>
     </body>
 </html>
