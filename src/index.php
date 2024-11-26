c136e8e67e2d635324ec33848f11c35200617ad4
Daniel Perez <workdevelopment001@gmail.com>
2024-11-25 23:01:18 -0600

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,14 +1,3 @@
 <?php
 
-//require 'public/index.php';
-
-$dsn = 'pdo:host=db;dbname=my_database';
-$username = 'user';
-$password = 'user_password';
-
-try {
-    $pdo = new PDO($dsn, $username, $password);
-    echo "Conexión exitosa a la base de datos.";
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
+echo "Hello B4A";
