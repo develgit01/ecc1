918510920dd2ba088280076b37b964d2a2e48ef9
Daniel Perez <workdevelopment001@gmail.com>
2024-11-27 01:06:42 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -10,5 +10,32 @@
           . phpversion()
           . '.</php';
     ?>
+    <hr>
+    <?php
+
+class ODBC{
+  //Conexion a la base de datos
+  public static function connection(){  
+    $host = 'ep-restless-bird-a2xx2dqs.eu-central-1.pg.koyeb.app';
+    $dbname = 'koyebdb';
+    $username = 'koyeb-adm';
+    $password = 'JIncLkX2yp9E';
+    try{
+      $pdo = new PDO("mysql:host=".$host.";dbname=".$dbname.";charset=utf8",$username,$password);
+    return $pdo;
+   }catch(PDOException $e){
+      echo $e->getMessage();
+   }
+}
+
+$dbconex = new ODBC;
+if($dbconex->conex()){
+  echo "Conectado";
+}else{
+  echo "Revise la conexión de su Base de datos.";
+}
+
+}
+?>
   </body>
 </html>
